1378dccec9c00fb451dfec990375b45b17acc9a1
unknown <jdevelopmp@gmail.com>
2026-01-29 08:53:29 -0500

    Login full responsive finished

---
+++ frontend/src/components/Login-Sigin/Button.jsx
@@ -0,0 +1,7 @@
+const Button = ({text}) => {
+    return (
+        <button className="px-14 py-3 text-white bg-linear-to-r from-[#1B49AE] from-40% via-[#3473FF] via-80% to-[#2563EB] to-100% rounded-xl font-medium sm:text-sm md:text-lg mt-5 cursor-pointer">{text}</button>
+    )
+}
+
+export default Button
---
+++ frontend/src/pages/SigIn.jsx
@@ -0,0 +1,14 @@
+const SigIn = () => {
+    return (
+        <div>
+            {/* Left */}
+            <div>
+
+            </div>
+            {/* Rigth */}
+            <div>
+
+            </div>
+        </div>
+    )
+}
